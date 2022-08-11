5fe953ef1b85837f4b6006cebee56d71678d4dfa
Max-Joseph Krempl <krempl@in.tum.de>
2022-08-11 18:34:57 +0200

    Refactored: Moved QR code generation to new struct QRGen

---
+++ QRGen/QRGen.swift
@@ -0,0 +1,85 @@
+//
+//  QRGen.swift
+//  QRGen
+//
+//  Created by Max-Joseph on 11.08.22.
+//
+
+import Foundation
+import CoreImage
+
+
+struct QRGen {
+	let inputFile: URL
+	let outputDir: URL
+	let correctionLevel: CorrectionLevel
+	
+	enum CorrectionLevel: String, ArgumentEnum {
+		case L, M, Q, H
+	}
+	
+	
+	/// Generate QR code and write output files
+	func run() throws {
+		// Read data from file
+		let inputData = try Data(contentsOf: inputFile)
+		
+		// Check output directory exists
+		var isDirectory: ObjCBool = false
+		guard FileManager.default.fileExists(atPath: outputDir.path, isDirectory: &isDirectory) && isDirectory.boolValue else {
+			exit(error: "No such output directory \"\(outputDir.path)\"")
+		}
+		
+		
+		// Create CoreImage filter
+		let filter = CIFilter(name: "CIQRCodeGenerator")!
+		filter.setValue(inputData, forKey: "inputMessage")
+		filter.setValue(correctionLevel.rawValue, forKey: "inputCorrectionLevel")
+		
+		guard let ciimage = filter.outputImage else {
+			exit(error: "Couldn't generate QR code")
+		}
+		
+		
+		// Write output files
+		let outputFileName = "\(inputFile.deletingPathExtension().lastPathComponent)_QR-\(correctionLevel)"
+		let outputFile = outputDir.appendingPathComponent(outputFileName)
+		let cicontext = CIContext()
+		
+		// PNG (1px scale)
+		try createPNG(cicontext: cicontext, ciimage: ciimage, outputFile: outputFile)
+		
+		// SVG
+		try createSVG(cicontext: cicontext, ciimage: ciimage, outputFile: outputFile)
+	}
+	
+	
+	private func createPNG(cicontext: CIContext, ciimage: CIImage, outputFile: URL) throws {
+		let outputFilePNG = outputFile.appendingPathExtension("png")
+		try cicontext.writePNGRepresentation(of: ciimage, to: outputFilePNG, format: .RGBA8, colorSpace: ciimage.colorSpace!)
+	}
+	
+	
+	private func createSVG(cicontext: CIContext, ciimage: CIImage, outputFile: URL) throws {
+		guard
+			let cgimage = cicontext.createCGImage(ciimage, from: ciimage.extent, format: .RGBA8, colorSpace: ciimage.colorSpace!),
+			let cfdata = cgimage.dataProvider?.data,
+			let dataPointer = CFDataGetBytePtr(cfdata),
+			cgimage.bitsPerPixel == 32 else {
+				exit(error: "Couldn't read bitmap data")
+		}
+		
+		let svg = BinaryPixelSVG(width: cgimage.width, height: cgimage.height)
+		
+		// Add pixels
+		svg.addPixels { point in
+			let isPixel = dataPointer[cgimage.bytesPerRow*point.y + point.x*4] == 0
+			guard isPixel else { return nil }
+			return .square
+		}
+		
+		// Write file
+		let outputFileSVG = outputFile.appendingPathExtension("svg")
+		try svg.content.write(to: outputFileSVG, atomically: true, encoding: .utf8)
+	}
+}
--- QRGen/main.swift
+++ QRGen/main.swift
@@ -6,15 +6,10 @@
 //
 
 import Foundation
-import CoreImage
 import ArgumentParser
 
 
 typealias ArgumentEnum = ExpressibleByArgument & CaseIterable
-
-enum CorrectionLevel: String, ArgumentEnum {
-	case L, M, Q, H
-}
 
 struct Arguments: ParsableCommand {
 	static var configuration: CommandConfiguration {
@@ -22,7 +17,7 @@ struct Arguments: ParsableCommand {
 	}
 	
 	@Option(name: .shortAndLong, help: ArgumentHelp("The QR code's correction level (parity)", valueName: "correction level"))
-	var level: CorrectionLevel = .M
+	var level: QRGen.CorrectionLevel = .M
 	
 	@Argument(help: ArgumentHelp("File containing the QR code's data", valueName: "input data file"), transform: URL.init(fileURLWithPath:))
 	var inputFile: URL
@@ -33,51 +28,11 @@ struct Arguments: ParsableCommand {
 
 // Parse arguments
 let arguments = Arguments.parseOrExit()
-let inputFile = arguments.inputFile
-let outputDir = arguments.outputDir
-let correctionLevel = arguments.level
 
-
-// Read data from file
-let inputData = try Data(contentsOf: inputFile)
-
-// Check output directory exists
-var isDirectory: ObjCBool = false
-guard FileManager.default.fileExists(atPath: outputDir.path, isDirectory: &isDirectory) && isDirectory.boolValue else {
-	exit(error: "No such output directory \"\(outputDir.path)\"")
-}
-
-
-// Create CoreImage filter
-let filter = CIFilter(name: "CIQRCodeGenerator")!
-filter.setValue(inputData, forKey: "inputMessage")
-filter.setValue(correctionLevel.rawValue, forKey: "inputCorrectionLevel")
-
-guard let ciimage = filter.outputImage else {
-	exit(error: "Couldn't generate QR code")
-}
-
-
-// Write output files
-let outputFileName = "\(inputFile.deletingPathExtension().lastPathComponent)_QR-\(correctionLevel)"
-let outputFile = outputDir.appendingPathComponent(outputFileName)
-
-// PNG (1px scale)
-let outputFilePNG = outputFile.appendingPathExtension("png")
-let context = CIContext()
-try context.writePNGRepresentation(of: ciimage, to: outputFilePNG, format: .RGBA8, colorSpace: ciimage.colorSpace!)
-
-// SVG
-guard
-	let cgimage = context.createCGImage(ciimage, from: ciimage.extent, format: .RGBA8, colorSpace: ciimage.colorSpace!),
-	let cfdata = cgimage.dataProvider?.data,
-	let dataPointer = CFDataGetBytePtr(cfdata),
-	cgimage.bitsPerPixel == 32 else {
-		exit(error: "Couldn't read bitmap data")
-}
-let outputFileSVG = outputFile.appendingPathExtension("svg")
-let svg = BinaryPixelSVG(width: cgimage.width, height: cgimage.height)
-svg.addPixels { x, y in
-	dataPointer[cgimage.bytesPerRow*y + x*4] == 0 ? .square : nil
-}
-try svg.content.write(to: outputFileSVG, atomically: true, encoding: .utf8)
+// Generate QR
+let qrGen = QRGen(
+	inputFile: arguments.inputFile,
+	outputDir: arguments.outputDir,
+	correctionLevel: arguments.level
+)
+try qrGen.run()
