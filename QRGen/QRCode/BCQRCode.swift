40abd974ced268802c96ab5a390df870eec14cd0
Max-Joseph Krempl <krempl@in.tum.de>
2022-08-16 05:26:34 +0200

    Fixed crash in `BCQRCode.cgimage` due to default color values' wrong color space (not my fault\!)

--- QRGen/QRCode/BCQRCode.swift
+++ QRGen/QRCode/BCQRCode.swift
@@ -6,12 +6,16 @@
 //
 
 import Foundation
+import AppKit
 import struct QRCodeGenerator.QRCode
 
 typealias BCQRCode = QRCode
 
 extension BCQRCode: QRCodeProtocol {
 	var cgimage: CGImage {
-		self.makeImage(border: 0).cgImage(forProposedRect: nil, context: nil, hints: nil)!
+		let black = NSColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+		let white = NSColor(red: 1, green: 1, blue: 1, alpha: 1.0)
+		let nsimage = self.makeImage(border: 0, foregroundColor: black, backgroundColor: white)
+		return nsimage.cgImage(forProposedRect: nil, context: nil, hints: nil)!
 	}
 }
