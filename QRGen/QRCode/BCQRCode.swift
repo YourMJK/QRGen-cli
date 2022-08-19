37b2ce3137ca11db3d358ce2ecd1c47bded713f1
Max-Joseph Krempl <krempl@in.tum.de>
2022-08-19 05:25:28 +0200

    `BCQRCode`: Changed image colors back to default values (now that the crash in the QRCodeGenerator dependency is fixed)

--- QRGen/QRCode/BCQRCode.swift
+++ QRGen/QRCode/BCQRCode.swift
@@ -13,9 +13,7 @@ typealias BCQRCode = QRCode
 
 extension BCQRCode: QRCodeProtocol {
 	var cgimage: CGImage {
-		let black = NSColor(red: 0, green: 0, blue: 0, alpha: 1.0)
-		let white = NSColor(red: 1, green: 1, blue: 1, alpha: 1.0)
-		let nsimage = self.makeImage(border: 0, foregroundColor: black, backgroundColor: white)
+		let nsimage = self.makeImage(border: 0)
 		return nsimage.cgImage(forProposedRect: nil, context: nil, hints: nil)!
 	}
 }
