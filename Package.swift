3975fd99e356e5b1d17dde5580074883cc5f6109
Max-Joseph Krempl <krempl@in.tum.de>
2023-02-19 02:27:30 +0100

    Changed `QRGen` dependency branch to "master"

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/YourMJK/swift-argument-parser", branch: "main"),
-		.package(url: "https://github.com/YourMJK/QRGen", branch: "module-cli-separation"),
+		.package(url: "https://github.com/YourMJK/QRGen", branch: "master"),
 	],
 	targets: [
 		.executableTarget(
